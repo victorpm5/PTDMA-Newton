78faaa0f0aabce2846bfd3182b1c1dc6e8337502
victor perez martos <victor.perez.martos@upcnet.es>
2020-04-11 20:33:09 +0200

    Create Android project + add speech recognition

---
+++ app/src/main/java/com/victor/newton/ui/home/HomeFragment.kt
@@ -0,0 +1,61 @@
+package com.victor.newton.ui.home
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.victor.newton.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var SPEECH_REQUEST_CODE: Int = 14
+    private var resultat: TextView? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        val micro: Button = root.findViewById(R.id.micro)
+        resultat = root.findViewById(R.id.speech)
+
+        micro.setOnClickListener {
+            graba()
+        }
+        return root
+    }
+
+    fun graba(){
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en")
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val results: List<String> = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = results.get(0)
+            resultat?.setText(spokenText)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
