5b3bbd00811841d4c44f375c941dc6aa00d47802
victor perez martos <victor.perez.martos@upcnet.es>
2020-04-13 12:34:03 +0200

    Add demo TTS function to Home Fragment

--- app/src/main/java/com/victor/newton/ui/home/HomeFragment.kt
+++ app/src/main/java/com/victor/newton/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +14,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.victor.newton.R
+import java.util.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(),TextToSpeech.OnInitListener {
 
     private lateinit var homeViewModel: HomeViewModel
     private var SPEECH_REQUEST_CODE: Int = 14
     private var resultat: TextView? = null
+    private var tts: TextToSpeech? = null
+    private var buttonReprodueix: Button? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -33,16 +39,46 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
 
-        val micro: Button = root.findViewById(R.id.micro)
+        //Declarem coses de la UI
+        val buttonGraba: FloatingActionButton = root.findViewById(R.id.button_graba)
+        buttonReprodueix = root.findViewById(R.id.button_reprodueix)
         resultat = root.findViewById(R.id.speech)
 
-        micro.setOnClickListener {
+        buttonReprodueix!!.isEnabled = false
+
+        //Declarem TTS
+        tts = TextToSpeech(root.context, this)
+
+        //Associem accions
+        buttonGraba.setOnClickListener {
             graba()
         }
+
+        buttonReprodueix!!.setOnClickListener {
+            reprodueixSo()
+        }
+
+
         return root
     }
 
-    fun graba(){
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            } else {
+                buttonReprodueix!!.isEnabled = true
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+
+    private fun graba(){
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en")
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
@@ -52,10 +88,25 @@ class HomeFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val results: List<String> = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            val spokenText = results.get(0)
-            resultat?.setText(spokenText)
+            val spokenText = results[0]
+            resultat?.text = spokenText
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
+
+    private fun reprodueixSo(){
+        val text = resultat!!.text.toString()
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+
+
 
 }
